747ecd2d7e31271039617ac0c13aabd4c1290aa7
MCanK <100578225+MCanK41@users.noreply.github.com>
2023-07-08 13:19:28 +0300

    3

--- Man.gd
+++ Man.gd
@@ -12,14 +12,21 @@ func _physics_process(_delta):
 	if Input.is_action_pressed("Right"):
 		velocity.x = SPEED
 		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("Run")
 	
 	elif Input.is_action_pressed("Left"):
 		velocity.x = -SPEED
 		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Run")
+	else:
+		$AnimatedSprite.play("Idle")
 	velocity.y = velocity.y +GRAVITY
 	
 	if Input .is_action_just_pressed("Jump") and .is_on_floor():
 		velocity.y = JUMPFORCE
+	
+	#if not is_on_floor():
+	#        $AnimatedSprite.play("Jump")
 	
 	velocity = move_and_slide(velocity,Vector2.UP)
 	
